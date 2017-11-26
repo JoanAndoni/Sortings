2c50d4ee4eb99a50ddb923ad0c4baefd4742293a
alejandratub <alitatc@hotmail.com>
2017-11-25 18:09:28 -0600

    shell sort con porcentaje de avance

--- sfml_graph.cpp
+++ sfml_graph.cpp
@@ -54,8 +54,12 @@ void InsertionSortDSC(vector<Data>* array, float * percentage);
 //SHELL
 void ShellSortASC(vector<Data>* array, float * percentage);
 void ShellSortDSC(vector<Data>* array, float * percentage);
+//HEAP
+ void heapSort(vector<Data>* array, float * percentage);
+  void filtrar(Data *inicio, Data *fin);
 //QUICK
-void quickSortASC(vector<Data>* array, float * percentage, Data *l, Data *h);
+int partition (vector<Data>* array, float * percentage, Data *low, Data *high);
+void QuickSortASC(vector<Data>* array, float * percentage, Data *l, Data *h);
 void swapt(Data *dat1, Data *dat2);
 
 
@@ -156,7 +160,7 @@ void userInput(ThreadParameters parameters)
     while(*parameters.windowStatus)
     {
 
-         cout<< endl<<"____________Visual Graph_____________"<< endl;
+         cout<< endl<<"---------Algoritmos de Ordenamiento-------"<< endl;
          cout<<"1. Agregar numero al arreglo"<< endl;
 				 cout<<"1.1. Agregar numero al arreglo"<< endl;
 				 cout<<"1.2. BubbleSort ASC"<< endl;
@@ -169,7 +173,7 @@ void userInput(ThreadParameters parameters)
 				 cout << "1.9 InsertionSortDSC" << endl;
 				 cout << "1.10 ShellSortASC" << endl;
 				 cout << "1.11 ShellSortASC" << endl;
-				 cout << "1.12 QuickSortASC" << endl;
+				 cout << "1.12 Heap" << endl;
 
          cout<<"2. Quit"<< endl;
          cout<<"Your selection: ";
@@ -237,6 +241,10 @@ void userInput(ThreadParameters parameters)
 				else if(selectedOption=="1.11")
 				{
 						ShellSortDSC(parameters.array,parameters.percentage);
+				}
+				else if(selectedOption=="1.12")
+				{
+						//heapSort(parameters.array,parameters.percentage);
 				}
 
         else if(selectedOption=="2")
@@ -467,54 +475,131 @@ void CocktailSortDSC(vector<Data>* array, float * percentage)
 }
 void InsertionSortASC(vector<Data>* array, float * percentage)
 {
+	float tam = array->size();
 	 for (int i = 1; i < array->size(); i++)
 	 {
-		 int j = i;
+		 		 int j = i;
 		 while ((j > 0) && (array->at(j).dat < array->at(j - 1).dat))
 		  {
 				swapt(&array->at(j),&array->at(j-1));
 			 j = j - 1;
 		 }
+		 if(i == tam-1){
+			*percentage = 100;
+		}
+		else{
+			*percentage = (float)i/(float)tam * 100;
+		}
+		printf("porcentaje %f\n", *percentage);
 	 }
  }
 
  void InsertionSortDSC(vector<Data>* array, float * percentage)
  {
+	 float tam = array->size();
  	 for (int i = 1; i < array->size(); i++)
  	 {
- 		 int j = i;
+ 		 		 int j = i;
  		 while ((j > 0) && (array->at(j).dat > array->at(j - 1).dat))
  		  {
  				swapt(&array->at(j),&array->at(j-1));
  			 j = j - 1;
  		 }
+ 		 if(i == tam-1){
+ 			*percentage = 100;
+ 		}
+ 		else{
+ 			*percentage = (float)i/(float)tam * 100;
+ 		}
+ 		printf("porcentaje %f\n", *percentage);
  	 }
   }
 void ShellSortASC(vector<Data>* array, float * percentage)
 {
-
+		float tam = array->size();
     int c, i, j, temp;
     for (c = (array->size())/2; c > 0; c /= 2)
 
         for (i = c; i < array->size(); i++)
-
-            for (j=i-c; j>=0 && array->at(j).dat > array->at(j + c).dat; j-=c)
+					{
+						for (j=i-c; j>=0 && array->at(j).dat > array->at(j + c).dat; j-=c)
 						{
 							swapt(&array->at(j),&array->at(j+c));
 
             }
+					}
+					if(i == 1){
+						*percentage = 100;
+					}
+					else{
+						*percentage = (float)c/(float)tam * 100;
+					}
+					printf("porcentaje %f\n", *percentage);
+
 }
 void ShellSortDSC(vector<Data>* array, float * percentage)
 {
-
+		float tam = array->size();
     int c, i, j, temp;
-    for (c = (array->size())/2; c > 0; c /= 2)
+    for (c = (array->size())/2; c >= 0; c /= 2)
 
         for (i = c; i < array->size(); i++)
-
-            for (j=i-c; j>=0 && array->at(j).dat < array->at(j + c).dat; j-=c)
+					{
+						for (j=i-c; j>=0 && array->at(j).dat < array->at(j + c).dat; j-=c)
 						{
 							swapt(&array->at(j),&array->at(j+c));
 
             }
+					}
+					if(i == 1){
+						*percentage = 100;
+					}
+					else{
+						*percentage = (float)c/(float)tam * 100;
+					}
+					printf("porcentaje %f\n", *percentage);
+
 }
+
+
+/* void heapSort(vector<Data>* array, float * percentage)
+    {
+			int Tam = array->size();
+        int comienzo = (Tam - 2) / 2;
+        while (comienzo >= 0)
+        {
+            filtrar(&array->at(comienzo),&array->at(Tam -1));
+            comienzo--;
+        }
+        int ultimo = Tam-1;
+        while(ultimo > 0)
+        {
+					swapt(&array->at(ultimo),&array->at(0));
+					filtrar(&array->at(0),&array->at(ultimo -1));
+          //  intercambiar(ultimo, 0);//intercambiar la raíz del heap con el último elemento
+          //  filtrar(0, ultimo - 1);//regresa el heap a que cumpla la condición del máximo padre para que sus hijos sean menores
+            ultimo--;//disminuye el tamaño del heap para que el valor máximo quede en su lugar
+        }
+    }
+
+
+ void filtrar(Data *inicio, Data *fin)
+    {
+        int raiz = inicio;
+        while((2*raiz + 1) <= fin) //mientras aún tenga una hoja la raíz
+        {
+            int hoja = 2*raiz + 1;//se va a la hoja de la izquierda
+            if((hoja + 1) <= fin && array->at(hoja).dat < array->at(hoja + 1).dat)//si la hoja tiene hermano hoja y es menor a él
+            {
+                hoja++;//entonces se va a la hoja de la derecha
+            }
+            if( array->at(raiz).dat < array->at(hoja).dat)//si se sale de orden
+            {
+							swapt(&array->at(raiz),&array->at(hoja));
+                raiz = hoja;//se asigna el valor de la hoja a la raíz para continuar revisando la hoja
+            }
+            else{
+                return;
+            }
+        }
+    }*/
